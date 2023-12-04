741f86c72ab5e02437ef9783a00a001ab7ba1c11
gublinmustang <152581245+gublinmustang@users.noreply.github.com>
2023-12-04 19:32:21 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 68 ms, Memory: 2680 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	int x;
+	int y;
+}coordinate;
+
+int compare(const void* a, const void* b);
+
+int main(void) {
+	int n;
+	coordinate* arr;
+	scanf("%d", &n);
+
+	arr = (coordinate*)malloc(sizeof(coordinate) * n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &arr[i].x, &arr[i].y);
+	}
+	qsort(arr, n, sizeof(coordinate), compare);
+	for (int i = 0; i < n; i++) {
+		printf("%d %d\n", arr[i].x, arr[i].y);
+	}
+
+	free(arr);
+	return 0;
+}
+
+int compare(const void* a, const void* b) {
+	coordinate cd1 = *(coordinate*)a;
+	coordinate cd2 = *(coordinate*)b;
+	if (cd1.x != cd2.x) return cd1.x - cd2.x;
+	else return cd1.y - cd2.y;
+}
