59a0a8452764fa1c3a86c5f2eab4ebf96deb1acd
gublinmustang <152581245+gublinmustang@users.noreply.github.com>
2024-01-02 21:05:57 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -6,35 +6,32 @@ int main(void) {
 
 	int n;
 	int i, j, k;
-	int count_1 = 0;
-	int count_2 = 0;
-
+	int count;
 	char input[51];
 
 	scanf("%d", &n);
 
 	for (i = 0; i < n; i++) {
+		count = 0;
 		scanf("%s", input);
 		k = strlen(input);
 		for (j = 0; j < k; j++) {
 			if (input[j] == '(') {
-				count_1++;
+				count++;
 			}
 			else if (input[j] == ')') {
-				count_2++;
+				count--;
 			}
-			if (count_1 < count_2) {
+			if (count < 0) {
 				break;
 			}
 		}
-		if (count_1 == count_2) {
+		if (count == 0) {
 			printf("YES\n");
 		}
 		else {
 			printf("NO\n");
 		}
-		count_1 = 0;
-		count_2 = 0;
 	}
 	return 0;
 }
