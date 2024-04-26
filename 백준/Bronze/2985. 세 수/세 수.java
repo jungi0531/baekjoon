2b5b9f94dc026893bb3e1e404d40b252bb486b2d
GoblinMustang <152581245+GoblinMustang@users.noreply.github.com>
2024-04-26 20:21:57 +0900

    [Bronze III] Title: 세 수, Time: 248 ms, Memory: 18560 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int c = scanner.nextInt();
+		
+		if (a + b == c) {
+			System.out.println(a + "+" + b + "=" + c);
+		}
+		else if (a - b == c) {
+			System.out.println(a + "-" + b + "=" + c);
+		}
+		else if (a * b == c) {
+			System.out.println(a + "*" + b + "=" + c);
+		}
+		else if (a / b == c) {
+			System.out.println(a + "/" + b + "=" + c);
+		}
+		else if (a == b + c) {
+			System.out.println(a + "=" + b + "+" + c);
+		}
+		else if (a == b - c) {
+			System.out.println(a + "=" + b + "-" + c);
+		}
+		else if (a == b * c) {
+			System.out.println(a + "=" + b + "*" + c);
+		}
+		else if (a == b / c) {
+			System.out.println(a + "=" + b + "/" + c);
+		}
+		scanner.close();
+	}
+}
