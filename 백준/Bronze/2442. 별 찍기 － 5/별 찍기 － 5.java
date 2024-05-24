f7423cfc3a1720b8914f8fd1703b14edea4f8e9e
GoblinMustang <152581245+GoblinMustang@users.noreply.github.com>
2024-05-24 21:01:58 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 400 ms, Memory: 20488 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int input = scanner.nextInt();
+		
+		for (int i = 0; i < input; i++) {
+			for (int j = i; j < input - 1; j++) 
+				System.out.print(" ");
+			for (int j = 0; j < 2 * i + 1; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+		scanner.close();
+	}
+}
