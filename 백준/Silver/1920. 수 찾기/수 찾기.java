e467426b1a7453f1056e9f5a25840c1f54ea7238
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2024-08-05 17:14:11 +0900

    [Silver IV] Title: 수 찾기, Time: 1692 ms, Memory: 186952 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -14,14 +14,25 @@ public class Main {
         N = scanner.nextInt();
         for (int i = 0; i < N; i++) {
             int input = scanner.nextInt();
-            if (Arrays.binarySearch(array, input) >= 0) {
-                System.out.println("1");
-            }
-            else {
-                System.out.println("0");
-            }
+            System.out.println(binarySearch(array, input, 0, array.length - 1));
         }
 
         scanner.close();
+    }
+    public static int binarySearch(int[] array, int input, int left, int right) {
+        int mid = (left + right) / 2;
+        if (left > right) {
+            return 0;
+        }
+        else if (array[mid] == input) {
+            return 1;
+        }
+        else if (array[mid] < input) {
+            return binarySearch(array, input, mid + 1, right);
+        }
+        else if (array[mid] > input) {
+            return binarySearch(array, input, left, mid - 1);
+        }
+        return 0;
     }
 }
