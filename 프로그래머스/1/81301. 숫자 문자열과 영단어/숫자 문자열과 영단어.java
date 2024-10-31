77b774e22db329672b9213c74185912a256868b3
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2024-10-31 18:10:05 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 1.06 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,34 +1,13 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        int i = 0;
-        String result = "";
-        String temp = "";
+        String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         
-        while (i < s.length()) {
-            if (Character.isDigit(s.charAt(i))) {
-                result += s.charAt(i);
-                i++;
-            }
-            else {
-                temp = "";
-                while (true) {
-                    temp += s.charAt(i);
-                    i++;
-                    if (temp.equals("one")) { result += "1"; break; }
-                    else if (temp.equals("two")) { result += "2"; break; }
-                    else if (temp.equals("three")) { result += "3"; break; }
-                    else if (temp.equals("four")) { result += "4"; break; }
-                    else if (temp.equals("five")) { result += "5"; break; }
-                    else if (temp.equals("six")) { result += "6"; break; }
-                    else if (temp.equals("seven")) { result += "7"; break; }
-                    else if (temp.equals("eight")) { result += "8"; break; }
-                    else if (temp.equals("nine")) { result += "9"; break; }
-                    else if (temp.equals("zero")) { result += "0"; break; }
-                }
-            }
+        for (int i = 0; i < numbers.length; i++) {
+            s = s.replaceAll(numbers[i], String.valueOf(i));
         }
-        answer = Integer.valueOf(result);
+        
+        answer = Integer.valueOf(s);
         return answer;
     }
 }
