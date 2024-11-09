b0cd1241b5dc6f88b0605aa2bf8c4ac5209bf905
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2024-11-09 22:25:45 +0900

    [Bronze II] Title: 단어순서 뒤집기, Time: 188 ms, Memory: 18212 KB -BaekjoonHub

--- 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
@@ -4,17 +4,20 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Stack<Integer> stack = new Stack<>();
+        Stack<String> stack = new Stack<>();
 
         int N = scanner.nextInt();
         scanner.nextLine();
         for (int i = 0; i < N; i++) {
             String input = scanner.nextLine();
             String[] temp = input.split(" ");
+            for (int j = 0; j < temp.length; j++) {
+                stack.push(temp[j]);
+            }
 
             System.out.print("Case #" + (i + 1) + ": ");
-            for (int j = 0; j < temp.length; j++) {
-                System.out.print(temp[temp.length - (j + 1)] + " ");
+            while (!stack.empty()) {
+                System.out.print(stack.pop() + " ");
             }
             System.out.println();
         }
