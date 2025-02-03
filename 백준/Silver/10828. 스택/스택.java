cd5b1a736c63f2522412d4cd81f7e9167b2a8936
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2025-02-04 00:40:12 +0900

    [Silver IV] Title: 스택, Time: 276 ms, Memory: 19188 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,40 +1,46 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.HashMap;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Stack<Integer> stack = new Stack<>();
 
-        int N = scanner.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            String input = scanner.next();
-            if (input.equals("push")) {
-                stack.push(scanner.nextInt());
-            } else if (input.equals("pop")) {
-                if (stack.empty()) {
-                    System.out.println(-1);
-                } else {
+        for (int i = 0; i < N; i++)
+        {
+            String[] input = br.readLine().split(" ");
+            if (input[0].equals("push"))
+            {
+                stack.push(Integer.parseInt(input[1]));
+            }
+            else if (input[0].equals("pop"))
+            {
+                if (stack.isEmpty())
+                    System.out.println("-1");
+                else
                     System.out.println(stack.pop());
-                }
-            } else if (input.equals("size")) {
+            }
+            else if (input[0].equals("size"))
+            {
                 System.out.println(stack.size());
-            } else if (input.equals("empty")) {
-                if (stack.empty()) {
-                    System.out.println(1);
-                } else {
-                    System.out.println(0);
-                }
-            } else if (input.equals("top")) {
-                if (stack.empty()) {
-                    System.out.println(-1);
-                } else {
+            }
+            else if (input[0].equals("empty"))
+            {
+                if (stack.isEmpty())
+                    System.out.println("1");
+                else
+                    System.out.println("0");
+            }
+            else if (input[0].equals("top"))
+            {
+                if (stack.isEmpty())
+                    System.out.println("-1");
+                else
                     System.out.println(stack.peek());
-                }
             }
         }
-
-        scanner.close();
     }
 }
