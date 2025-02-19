d6ff63528797e33bbbcaa52853891aa45e1151a5
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2025-02-19 13:35:32 +0900

    [Bronze I] Title: 세준세비, Time: 420 ms, Memory: 67736 KB -BaekjoonHub

--- 백준/Bronze/1524. 세준세비/세준세비.java
+++ 백준/Bronze/1524. 세준세비/세준세비.java
@@ -1,37 +1,41 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for (int i = 0; i < T; i++) {
-            br.readLine();  // 공백 제거
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-
-            int sMax = 0;
-            int bMax = 0;
-
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                int input = Integer.parseInt(st.nextToken());
-                if (input > sMax) sMax = input;
+        try {
+            // 테스트 케이스 수 입력 받기
+            int T = Integer.parseInt(br.readLine());
+            for (int i = 0; i < T; i++) {
+                // 세준 세비 병사 입력 받기
+                br.readLine();
+                String[] input = br.readLine().split(" ");
+                // 결국엔 가장 강한 병사가 있는 쪽이 이기기에, 입력 값을 반복문으로 돌면서 MAX 값 구하기
+                int S = Integer.MIN_VALUE;
+                int B = Integer.MIN_VALUE;
+                String[] temp = br.readLine().split(" ");
+                for (int ii = 0; ii < Integer.parseInt(input[0]); ii++) {
+                    if (Integer.parseInt(temp[ii]) > S)
+                        S = Integer.parseInt(temp[ii]);
+                }
+                String[] temp2 = br.readLine().split(" ");
+                for (int ii = 0; ii < Integer.parseInt(input[1]); ii++) {
+                    if (Integer.parseInt(temp2[ii]) > B)
+                        B = Integer.parseInt(temp2[ii]);
+                }
+                // 누가 이긴지 출력 -> MAX 비교 후 큰 쪽 출력
+                if (S >= B)
+                    bw.write("S\n");
+                else
+                    bw.write("B\n");
             }
-
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                int input = Integer.parseInt(st.nextToken());
-                if (input > bMax) bMax = input;
-            }
-
-            if (sMax >= bMax) {
-                System.out.println("S");
-            } else {
-                System.out.println("B");
-            }
+            bw.flush();
+            bw.close();
+            br.close();
+        } catch(IOException e) {
+            e.printStackTrace();
         }
     }
 }
