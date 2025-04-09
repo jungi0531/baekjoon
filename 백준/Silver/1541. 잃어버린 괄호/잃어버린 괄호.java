05da3104317f6711a699d1c835a60f8a45c4db08
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2025-04-09 11:23:18 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 108 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -6,17 +6,21 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int result = 0;
-        // 괄호 쳐서 식 값 최소 -> - 나오면 다시 - 나올 때까지 괄호
+        // 괄호 쳐서 식 값 최소 -> - 나오면 다시 - 나올 때까지 괄호(마이너스 값을 최대로 묶기)
         // 먼저 -로 스플릿 하고 각 스플릿 덩어리 값 구한 뒤 첫 값에서 빼기
         String input = br.readLine();
+        // -로 덩어리 나누기
         String[] splitMinus = input.split("-");
+        // -로 나눈 각 덩어리 안의 숫자 합을 저장할 정수 배열
         int[] arr = new int[splitMinus.length];
         for (int i = 0; i < splitMinus.length; i++) {
+            // 각 덩어리들의 숫자 합 arr에 저장
             String[] splitPlus = splitMinus[i].split("\\+");
             for (int ii = 0; ii < splitPlus.length; ii++) {
                 arr[i] += Integer.parseInt(splitPlus[ii]);
             }
         }
+        // 첫 값에서 -로 나눈 덩어리들 숫자 합 빼기
         result = arr[0];
         for (int i = 1; i < arr.length; i++) {
             result -= arr[i];
