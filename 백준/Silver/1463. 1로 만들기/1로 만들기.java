22be12b7a7a0dd68d244b9a25f599607f9581855
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2025-07-13 20:55:20 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 18204 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,24 +1,24 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = scanner.nextInt();
-        int[] array = new int[n + 1];
-
-        array[1] = 0;
-        for (int i = 2; i <= n; i++) {
-            array[i] = array[i - 1] + 1;
-            if (i % 2 == 0) {
-                array[i] = Math.min(array[i], array[i / 2] + 1);
-            }
-            if (i % 3 == 0) {
-                array[i] = Math.min(array[i], array[i / 3] + 1);
-            }
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        dp[0] = -1;
+        for (int i = 1; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0)
+                dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
+            if (i % 3 == 0)
+                dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
         }
-        System.out.println(array[n]);
-
-        scanner.close();
+        // output
+        bw.write(String.valueOf(dp[N]) + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
