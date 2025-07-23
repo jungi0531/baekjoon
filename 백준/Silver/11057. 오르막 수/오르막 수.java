b5850ab3692b9f84412909b3962f943e0becfe14
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2025-07-23 22:10:49 +0530

    [Silver I] Title: 오르막 수, Time: 112 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -5,6 +5,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int MOD = 10007;
         int result = 0;
         int N = Integer.parseInt(br.readLine());
         int[][] dp = new int[N + 1][10];
@@ -16,16 +17,16 @@ public class Main {
             for (int ii = 0; ii < 10; ii++) {
                 dp[i][0] += dp[i - 1][ii];
             }
-            dp[i][0] %= 10007;
+            dp[i][0] %= MOD;
             for (int ii = 1; ii < 10; ii++) {
-                dp[i][ii] = (dp[i][ii - 1] - dp[i - 1][ii - 1]) + 10007;
-                dp[i][ii] %= 10007;
+                dp[i][ii] = (dp[i][ii - 1] - dp[i - 1][ii - 1]) + MOD;
+                dp[i][ii] %= MOD;
             }
         }
 
         for (int i = 0; i < 10; i++) {
             result += dp[N][i];
-            result %= 10007;
+            result %= MOD;
         }
         bw.write(String.valueOf(result) + "\n");
         bw.flush();
