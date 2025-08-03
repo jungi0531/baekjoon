2f0da9a5d27364c4f94881fb026e79da0cfc230a
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2025-08-03 11:27:56 +0530

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 116 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        // 입력
         int N = Integer.parseInt(br.readLine());
         String[] input = br.readLine().split(" ");
         int[] arr = new int[N];
@@ -11,6 +12,10 @@ public class Main {
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(input[i]);
         }
+
+        // ii 인덱스의 값이 i 인덱스에 있는 값보다 크면 감소하는 수열이 될 수 있으므로
+        // ii 인덱스에 i 인덱스의 있는 값을 추가한 길이인 dp[ii] + 1과 현재 dp[i] 값을 비교한다.
+        // <- 현재 dp[i] 값에는 이와 같이 전에 비교된 값이 들어있을 수 있음
         for (int i = 1; i < N; i++) {
             for (int ii = 0; ii < i; ii++) {
                 if (arr[i] < arr[ii])
@@ -18,6 +23,7 @@ public class Main {
             }
         }
 
+        // 가장 긴 부분 찾아 출력
         int max = 0;
         for (int i = 0; i < N; i++) {
             if (max < dp[i])
