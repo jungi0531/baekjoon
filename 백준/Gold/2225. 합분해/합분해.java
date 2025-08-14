af076bbb8e45d38057f92daf75fa87144e3801b2
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2025-08-14 10:55:15 +0530

    [Gold V] Title: 합분해, Time: 196 ms, Memory: 18380 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        // 입력
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        // 초기화
+        int[][] dp = new int[N + 1][K + 1];
+        for (int i = 1; i <= N; i++) {
+            dp[i][1] = 1;
+        }
+        for (int i = 1; i <= K; i++) {
+            dp[0][i] = 1;
+            dp[1][i] = i;
+        }
+
+        //
+        for (int i = 2 ; i <= N; i++) {
+            for (int ii = 1; ii <= K; ii++) {
+                for (int iii = 0; iii <= i; iii++) {
+                    dp[i][ii] += dp[iii][ii - 1];
+                    dp[i][ii] %= 1_000_000_000;
+                }
+            }
+        }
+
+        // 출력
+        System.out.println(dp[N][K]);
+    }
+}
