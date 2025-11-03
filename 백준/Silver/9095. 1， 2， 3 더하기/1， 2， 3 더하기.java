619910ec08b232efff0afd514287b64d1e4e512a
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2025-11-03 22:39:50 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,33 +1,27 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        //
         int T = Integer.parseInt(br.readLine());
-        int[] dp;
+        int[] n = new int[T];
+        int max = 0;
         for (int i = 0; i < T; i++) {
-            int n = Integer.parseInt(br.readLine());
-            dp = new int[n + 3];
-            dp[1] = 1;
-            dp[2] = 2;
-            dp[3] = 4;
-            for (int ii = 4; ii <= n; ii++) {
-                dp[ii] = dp[ii - 1] + dp[ii - 2] + dp[ii - 3];
-            }
-            bw.write(String.valueOf(dp[n]) + "\n");
-
-            // 1
-            // 1 1, 2
-            // 1 1 1, 1 2, 2 1, 3
-
-            // 1 1 1 1
-            // 1 1 2, 2 2
-            // 1 1 1 1, 1 1 2, 2 1 1, 1 3
+            n[i] = Integer.parseInt(br.readLine());
+            max = Math.max(n[i], max);
         }
-        bw.flush();
-        bw.close();
-        br.close();
+        int[] dp = new int[max + 3];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i <= max; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        for (int i = 0; i < T; i++) {
+            System.out.println(dp[n[i]]);
+        }
     }
 }
