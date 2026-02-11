129b0e61f1c998ae8dff715ad9276f9b2fc97b7f
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-02-11 17:01:35 +0900

    [level 2] Title: 피로도, Time: 5.45 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,29 +1,23 @@
 class Solution {
-    static int answer;
+    static int count;
     static boolean[] visited;
     
     public int solution(int k, int[][] dungeons) {
-        answer = 0;
+        count = -1;
         visited = new boolean[dungeons.length];
         
-        for (int i = 0; i < dungeons.length; i++) {
-            if (dungeons[i][0] <= k) {
-                visited[i] = true;
-                backtracking(k - dungeons[i][1], dungeons, 1);
-                visited[i] = false;
-            }
-        }
+        backtracking(k, dungeons, 0);
         
-        return answer;
+        return count;
     }
-    public void backtracking(int k, int[][] dungeons, int count) {
+    public void backtracking(int k, int[][] dungeons, int cur) {
         for (int i = 0; i < dungeons.length; i++) {
             if (!visited[i] && dungeons[i][0] <= k) {
                 visited[i] = true;
-                backtracking(k - dungeons[i][1], dungeons, count + 1);
+                backtracking(k - dungeons[i][1], dungeons, cur + 1);
                 visited[i] = false;
             }
         }
-        answer = Math.max(answer, count);
+        count = Math.max(count, cur);
     }
 }
