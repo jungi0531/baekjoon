a5d06705fce857927360e01c057bb4896600be25
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-02-11 16:08:18 +0900

    [level 2] Title: 주식가격, Time: 50.62 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,28 +2,24 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
+        // 스택으로 떨어졌으면 어디까지 떨어졌는지 확인
+        // 
+        // 그냥 해당 인덱스 넣다가 수가 줄어드는 인덱스 만나면 해당
         int[] answer = new int[prices.length];
-        
-        Stack<int[]> stack = new Stack<>();
-        // 먼가 스택인 거 같은데
-        // 계속 스택에 넣다가 더 작은 수가 나오면 pop하고 넣어서 해당 인덱스에 기록?
-        // prices 정보만으로 하는 게 가능한가? push 당시 인덱스도 필요할 거 같은데
+        Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < prices.length; i++) {
-            // 주식이 떨어져있으면 근데 이거는 멀리는 못 보는데
-            if (!stack.isEmpty() && stack.peek()[1] > prices[i]) {
-                while (!stack.isEmpty() && stack.peek()[1] > prices[i]) {
-                    int[] remove = stack.pop();
-                    answer[remove[0]] = i - (remove[0]);
+            if (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                    answer[stack.peek()] = i - stack.peek();
+                    stack.pop();
                 }
             }
-            stack.push(new int[]{i, prices[i]});
-            
+            stack.push(i);
         }
-        
-        // 마지막까지 안 떨어진 주식들
-        while (!stack.isEmpty()) {
-            int[] remove = stack.pop();
-            answer[remove[0]] = prices.length - (remove[0] + 1);
+        for (int i = 0; i < prices.length; i++) {
+            if (answer[i] == 0) {
+                answer[i] = prices.length - (i + 1);
+            }
         }
         
         return answer;
