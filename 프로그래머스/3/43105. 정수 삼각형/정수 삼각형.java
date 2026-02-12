ecaf284e06dd208d366a2e9bdfd489588e615c52
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-02-12 12:49:28 +0900

    [level 3] Title: 정수 삼각형, Time: 7.86 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,25 +1,25 @@
 class Solution {
     public int solution(int[][] triangle) {
+        // 흠 max 값을 받아오도록 하면 되잖아
+        // i면 그 다음의 i와 i + 1에 드갈 수 있다.
+        // 가장자리 부분을 먼저 해놔야 하나
+        int answer = 0;
         
+        // 가장자리 미리 해놓기
         for (int i = 1; i < triangle.length; i++) {
-            for (int ii = 0; ii < triangle[i].length; ii++) {
-                if (ii == 0) {
-                    triangle[i][ii] += triangle[i - 1][ii];
-                } else if (ii == triangle[i].length - 1) {
-                    triangle[i][ii] += triangle[i - 1][ii - 1];
-                } else {
-                    // 가에가 아니면
-                    triangle[i][ii] += Math.max(triangle[i - 1][ii], triangle[i - 1][ii - 1]);
-                }
+            triangle[i][0] += triangle[i - 1][0];
+            triangle[i][triangle[i].length - 1] += triangle[i - 1][triangle[i - 1].length - 1];
+        }
+        for (int i = 1; i < triangle.length; i++) {
+            for (int ii = 1; ii < triangle[i].length - 1; ii++) {
+                triangle[i][ii] += Math.max(triangle[i - 1][ii - 1], triangle[i - 1][ii]);
             }
         }
         
-        int max = 0;
-        for (int i = 0; i < triangle[triangle.length - 1].length - 1; i++) {
-            if (max < triangle[triangle.length - 1][i]) {
-                max = triangle[triangle.length - 1][i];
-            }
+        for (int i = 0; i < triangle[triangle.length - 1].length; i++) {
+            answer = Math.max(answer, triangle[triangle.length - 1][i]);
         }
-        return max;
+        
+        return answer;
     }
 }
