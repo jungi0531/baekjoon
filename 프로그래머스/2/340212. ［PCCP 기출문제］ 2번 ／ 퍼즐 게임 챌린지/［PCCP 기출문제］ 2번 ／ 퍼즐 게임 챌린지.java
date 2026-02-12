291b41aba32d8a7c25892b046ad679a00438e18b
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-02-12 14:08:35 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 64.72 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,32 +1,27 @@
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
-        // 이진 탐색이다. 특정 level이라고 가정하고 걸린 시간이 limit보다 크면 level을 올리고 낮으면 level을 내리고 이진 탐색으로
-        // 레벨 초기값 설정 -> 해결할 수 있는 최소 레벨을 찾아야 함
+        // 이분 탐색으로 최솟값을 찾아야한다.
+        // 그럼 특정 값으로 시작 후 이분 탐색으로 값을 맞추어 나가야 함
+        long level = 0;
         long start = 1;
-        long end = 100_000;
-        int answer = 0;
+        long end = 100_000_000_000_000L;
         
         while (start <= end) {
-            long curTime = 0;
-            long level = (start + end) / 2;
-            for (int i = 0; i < diffs.length; i++) {
-                if (diffs[i] <= level) {
-                    // 맞음
-                    curTime += times[i];
-                } else {
-                    // 틀림
-                    curTime += (diffs[i] - level) * (times[i - 1] + times[i]) + times[i];
+            level = (start + end) / 2;
+            long sum = times[0];    // diffs[0] = 1
+            for (int i = 1; i < diffs.length; i++) {
+                sum += times[i];
+                if (diffs[i] > level) {
+                    sum += (diffs[i] - level) * (times[i - 1] + times[i]);
                 }
-                if (curTime > limit) break;
             }
-            if (curTime <= limit) {
-                answer = (int)level;
+            if (limit >= sum) {
                 end = level - 1;
             } else {
                 start = level + 1;
             }
         }
         
-        return answer;
+        return (int)start;
     }
 }
