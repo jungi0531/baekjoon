60696ce71c19ab3c57a2f49aa9ef6119441c8208
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-02-12 12:39:24 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 93.37 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,56 +1,60 @@
 import java.util.*;
 
 class Solution {
-    static final int[] x = {1, 0, -1, 0};
-    static final int[] y = {0, -1, 0, 1};
     static int[] result;
+    static boolean[][] visited;
+    static final int[] dx = {1, 0, -1, 0};
+    static final int[] dy = {0, -1, 0 ,1};
     
     public int solution(int[][] land) {
         int answer = 0;
         result = new int[land[0].length];
-        boolean[][] visited = new boolean[land.length][land[0].length];
+        visited = new boolean[land.length][land[0].length];
         
-        for (int i = 0; i < land.length; i++) {
-            for (int ii = 0; ii < land[0].length; ii++) {
-                if (!visited[i][ii] && land[i][ii] == 1) {
-                    bfs(land, visited, i, ii);
-                }
-            }
+        for (int i = 0; i < land[0].length; i++) {
+            searchCol(land, i);
         }
-        
-        for (int i = 0; i < result.length; i++) {
+        for (int i = 0; i < land[0].length; i++) {
+            // System.out.println(result[i]);
             answer = Math.max(answer, result[i]);
         }
         
         return answer;
     }
-    public void bfs(int[][] land, boolean[][] visited, int row, int col) {
-        // 현재 덩어리의 크기를 구하고 해당 덩어리가 포함되는 열에 덩어리 크기 더하기
+    public void searchCol(int[][] land, int col) {
+        // 덩어리를 찾아도 그냥 방문처리만 하고 말고 덩어리 크기를 구해놓은 뒤
+        // 해당 덩어리가 있는 col에 해당 덩어리의 크기를 모두 더해야 함
+        for (int i = 0; i < land.length; i++) {
+            if (!visited[i][col] && land[i][col] == 1) {
+                bfs(land, i, col);
+            }
+        }
+    }
+    public void bfs(int[][] land, int row, int col) {
         int count = 0;
         Queue<int[]> q = new LinkedList<>();
         HashSet<Integer> hash = new HashSet<>();
         
-        q.add(new int[]{row, col});
         visited[row][col] = true;
         count++;
+        q.add(new int[]{row, col});
         hash.add(col);
         
         while (!q.isEmpty()) {
             int[] remove = q.poll();
             for (int i = 0; i < 4; i++) {
-                int curX = remove[1] + x[i];
-                int curY = remove[0] + y[i];
-                if (0 <= curX && curX < land[0].length &&
-                    0 <= curY && curY < land.length &&
-                    !visited[curY][curX] && land[curY][curX] == 1) {
-                    q.add(new int[]{curY, curX});
+                int x = remove[1] + dx[i];
+                int y = remove[0] + dy[i];
+                if (0 <= x && x < land[0].length &&
+                    0 <= y && y < land.length &&
+                    !visited[y][x] && land[y][x] == 1) {
+                    visited[y][x] = true;
                     count++;
-                    visited[curY][curX] = true;
-                    hash.add(curX);
+                    q.add(new int[]{y, x});
+                    hash.add(x);
                 }
             }
         }
-        
         for (int i : hash) {
             result[i] += count;
         }
