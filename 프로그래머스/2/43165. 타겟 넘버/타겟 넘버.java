55e5ab197a400463008bcebdd47355f77ae1f4a4
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-02-12 21:32:07 +0900

    [level 2] Title: 타겟 넘버, Time: 15.50 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,45 +1,24 @@
-import java.util.*;
-
 class Solution {
-    static int count = 0;
-    
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-        boolean[] visited = new boolean[numbers.length];
+        int answer = dfs(numbers, target, 0, 0);
         
-        // 타겟이 -니까 무조건 마이너스로 해야하고
-        for (int i = 0; i < numbers.length; i++) {
-            visited[i] = true;
-            numbers[i] = -numbers[i];
-            dfs(visited, numbers, target, i);
-            numbers[i] = -numbers[i];
-            visited[i] = false;
+        return answer;
+    }
+    public int dfs(int[] numbers, int target, int curIndex, int sum) {
+        int count = 0;
+        
+        if (curIndex >= numbers.length) {
+            if (target == sum) return 1;
+            return 0;
         }
         
+        count += dfs(numbers, target, curIndex + 1, sum + numbers[curIndex]);
+        
+        numbers[curIndex] = -numbers[curIndex];
+        count += dfs(numbers, target, curIndex + 1, sum + numbers[curIndex]);
+        numbers[curIndex] = -numbers[curIndex];
+        
+        // System.out.println("curIndex: " + curIndex + " count: " + count);
         return count;
-    }
-    public void dfs(boolean[] visited, int[] arr, int target, int cur) {
-        if (getSum(arr) == target) {
-            //System.out.println(Arrays.toString(arr));
-            count++;
-            return ;
-        }
-        for (int i = cur; i < arr.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                arr[i] = -arr[i];
-                dfs(visited, arr, target, i);
-                arr[i] = -arr[i];
-                visited[i] = false;
-            }
-        }
-    }
-    public int getSum(int[] arr) {
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
-        }
-        
-        return sum;
     }
 }
