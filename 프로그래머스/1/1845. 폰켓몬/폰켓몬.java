78a72cc1772db135211edff17d0ccbcb0fffbbe0
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-02-12 14:54:26 +0900

    [level 1] Title: 폰켓몬, Time: 7.89 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,20 +1,14 @@
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        
-        HashSet<Integer> hash = new HashSet<>();
-        
+        // 해시맵에 다 넣고 answer = Math.max(nums.length, hash.size());
+        HashMap<Integer, Integer> hash = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            hash.add(nums[i]);
+            hash.put(nums[i], hash.getOrDefault(nums[i], 0) + 1);
         }
         
-        answer = hash.size();
-        
-        if (answer >= (nums.length / 2)) {
-            answer = nums.length / 2;
-        }
+        int answer = Math.min(nums.length / 2, hash.size());
         
         return answer;
     }
