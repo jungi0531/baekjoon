35719ee807e70fcd05b73c3c25865baea2917ca5
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-03-11 11:40:22 +0900

    [level 2] Title: 롤케이크 자르기, Time: 280.90 ms, Memory: 188 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -2,20 +2,21 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] topping) {
-        // 두 개의 해시를 만들고 한 해시에서 다른 해시로 옮겨담기
+        // 해시맵과 셋 활용
         int answer = 0;
-        HashSet<Integer> hash1 = new HashSet<>();
-        HashMap<Integer, Integer> hash2 = new HashMap<>();
-        // 우선 두 번째 해시에 다 넣고
+        HashMap<Integer, Integer> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
+        
         for (int i = 0; i < topping.length; i++) {
-            hash2.put(topping[i], hash2.getOrDefault(topping[i], 0) + 1);
+            map.put(topping[i], map.getOrDefault(topping[i], 0) + 1);
         }
-        // 해시에서 하나씩 빼서 다른 해시에 넣고 개수 비교
+        
         for (int i = 0; i < topping.length; i++) {
-            hash1.add(topping[i]);
-            hash2.put(topping[i], hash2.get(topping[i]) - 1);
-            if (hash2.get(topping[i]) == 0) hash2.remove(topping[i]);
-            if (hash1.size() == hash2.size()) answer++;
+            map.put(topping[i], map.get(topping[i]) - 1);
+            if (map.get(topping[i]) == 0) map.remove(topping[i]);
+            set.add(topping[i]);
+            
+            if (map.size() == set.size()) answer++;
         }
         
         return answer;
