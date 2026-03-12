358ee3cf7d812fd324c9ce7309f3c55b5c1a459d
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-03-12 14:28:23 +0900

    [level 3] Title: 네트워크, Time: 0.39 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -5,43 +5,38 @@ class Solution {
     static ArrayList<Integer>[] arr;
     
     public int solution(int n, int[][] computers) {
-        // 뭐야 그래프 탐색에 그냥 몇 뭉티 있는지 세는 거 아녀?
-        // ArrayList에 추가해놓고 계속 돌기
+        // 이어진 그래프 세기
         int answer = 0;
         visited = new boolean[n];
+        
         arr = new ArrayList[n];
         for (int i = 0; i < n; i++) {
             arr[i] = new ArrayList<>();
         }
-        
         for (int i = 0; i < n; i++) {
             for (int ii = 0; ii < n; ii++) {
-                if (computers[i][ii] == 1 && i != ii) {
+                if (computers[i][ii] == 1) {
                     arr[i].add(ii);
-                    arr[ii].add(i);
                 }
             }
         }
         
-        // 여기서 탐색해야하네..
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                // 여기서 다 visitied로 바꾸기
-                dfs(i, n);
                 answer++;
+                dfs(i);
             }
         }
         
         return answer;
     }
-    public void dfs(int cur, int n) {
+    public void dfs(int cur) {
         visited[cur] = true;
         
-        for (int next : arr[cur]) {
-            if (visited[next] == false) {
-                dfs(next, n);
+        for (int i = 0; i < arr[cur].size(); i++) {
+            if (!visited[arr[cur].get(i)]) {
+                dfs(arr[cur].get(i));
             }
         }
-        
     }
 }
