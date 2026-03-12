92bc7360cfd0c9768fb05e471cd41ac7ca91710a
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-03-12 12:21:56 +0900

    [level 2] Title: 타겟 넘버, Time: 6.80 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,24 +1,21 @@
 class Solution {
+    static int count;
+    
     public int solution(int[] numbers, int target) {
-        int answer = dfs(numbers, target, 0, 0);
+        count = 0;
         
-        return answer;
+        // 현재가 +인 경우, -인 경우 있음
+        dfs(numbers, target, 0, 0);
+        
+        return count;
     }
-    public int dfs(int[] numbers, int target, int curIndex, int sum) {
-        int count = 0;
-        
+    public void dfs(int[] numbers, int target, int curIndex, int sum) {
         if (curIndex >= numbers.length) {
-            if (target == sum) return 1;
-            return 0;
+            if (target == sum) count++;
+            return;
         }
         
-        count += dfs(numbers, target, curIndex + 1, sum + numbers[curIndex]);
-        
-        numbers[curIndex] = -numbers[curIndex];
-        count += dfs(numbers, target, curIndex + 1, sum + numbers[curIndex]);
-        numbers[curIndex] = -numbers[curIndex];
-        
-        // System.out.println("curIndex: " + curIndex + " count: " + count);
-        return count;
+        dfs(numbers, target, curIndex + 1, sum + numbers[curIndex]);
+        dfs(numbers, target, curIndex + 1, sum - numbers[curIndex]);
     }
 }
