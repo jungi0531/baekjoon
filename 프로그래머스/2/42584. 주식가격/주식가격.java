1169b1bebfad1d011de54e2f62a40f60200f3af2
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-03-26 21:52:32 +0900

    [level 2] Title: 주식가격, Time: 36.51 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,24 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        // 스택으로 떨어졌으면 어디까지 떨어졌는지 확인
-        // 
-        // 그냥 해당 인덱스 넣다가 수가 줄어드는 인덱스 만나면 해당
+        // 스택을 활용해서 스택에 계속 넣다가? 낮아지면 나온 숫자와 같아질 때까지 숫자 빼기
+        // 스택의 첫 값은 인덱스, 두 번째 값은 값
+        Stack<int[]> stack = new Stack<>();
         int[] answer = new int[prices.length];
-        Stack<Integer> stack = new Stack<>();
+        
         for (int i = 0; i < prices.length; i++) {
-            if (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-                while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-                    answer[stack.peek()] = i - stack.peek();
-                    stack.pop();
-                }
+            // 스택에 들어있는 값보다 현재 값이 작으면 떨어진 거라서
+            // 스택에 들어있는 값과 같거나 클 때까지 빼기
+            while (!stack.isEmpty() && stack.peek()[1] > prices[i]) {
+                int[] remove = stack.pop();
+                answer[remove[0]] = i - remove[0];
             }
-            stack.push(i);
+            stack.push(new int[]{i, prices[i]});
         }
-        for (int i = 0; i < prices.length; i++) {
-            if (answer[i] == 0) {
-                answer[i] = prices.length - (i + 1);
-            }
+        while (!stack.isEmpty()) {
+            int[] remove = stack.pop();
+            answer[remove[0]] = (prices.length - 1) - remove[0];
         }
         
         return answer;
