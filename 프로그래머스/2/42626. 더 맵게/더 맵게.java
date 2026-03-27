4c4625de5195f5726b0c2fdb56b73e16ea65c411
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-03-27 14:02:37 +0900

    [level 2] Title: 더 맵게, Time: 1550.89 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,26 +1,23 @@
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
+        // 해시에 넣어서 두 개씩 빼고 넣기 peek() >= K 일 때까지
         int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Long> pq = new PriorityQueue<>();
         
         for (int i = 0; i < scoville.length; i++) {
-            pq.add(scoville[i]);
+            pq.add((long)scoville[i]);
         }
         
-        while (true) {
-            if (pq.peek() >= K) break;
-            int temp = pq.poll();
-            if (pq.isEmpty()) {
-                answer = -1;
-                break;
-            }
-            temp = temp + (pq.poll() * 2);
-            pq.add(temp);
+        while (pq.size() >= 2 && pq.peek() < K) {
+            Long a = pq.poll();
+            Long b = pq.poll();
+            pq.add(a + (b * 2));
             answer++;
         }
         
+        if (pq.peek() < K) return -1;
         return answer;
     }
 }
