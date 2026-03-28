840a88bd667c2bbd10cf8ad7330617663ff5c263
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-03-28 18:18:52 +0900

    [level 2] Title: H-Index, Time: 0.70 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,18 +1,33 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
+        // n 이상 수가 n 개인 n 찾기
+        // 최댓값이라고 하니 0부터 시작해서 늘려가며 최대를 찾기
+        // 1이면 1이상 수가 1개
+        // 2이면 2이상 수가 2개
+        // 그럼 오름차순 정렬하고
+        // 특정 인덱스의 수 i일 때 i 포함 뒤의 수가 i개 있으면 됨
+        // 특정 인덱스의 수 i일 때 i 포함 뒤의 수가 i보다 작으면 탈출
+        // 0 0 4 4 의 경우
+        // 1 이상 1개 이상
+        // 2 이상 2개 이상 <-
+        // 3 이상 3개 이상 아님
         int answer = 0;
-    
-        Arrays.sort(citations);
         
-        for (int i = 0; i < citations.length; i++) {
-            //System.out.println(citations[i]);
-            if (citations[i] >= citations.length - i) {
-                answer = citations.length - i;
-                break;
-            }
+        Arrays.sort(citations);
+        for (int i = citations.length - 1; i >= 0; i--) {
+            if (citations[i] < citations.length - i) break;
+            answer = citations.length - i;
         }
+        
+        // for (int i = 0; i < citations.length; i++) {
+        //     if (citations[i] >= citations.length - i + 1) {
+        //         break;
+        //     }
+        //     //System.out.println(i);
+        //     answer = citations.length - i;
+        // }
         
         return answer;
     }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 88.7 MB, 시간: 0.82 ms
+메모리: 89 MB, 시간: 0.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 28일 19:42:04
+2026년 03월 28일 18:18:49
 
 ### 문제 설명
 
