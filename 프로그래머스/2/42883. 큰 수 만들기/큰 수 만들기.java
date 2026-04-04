854a29229c362fe3beac9f425845775e5295e6b2
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-04-04 19:24:00 +0900

    [level 2] Title: 큰 수 만들기, Time: 73.85 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -2,20 +2,26 @@ import java.util.*;
 
 class Solution {
     public String solution(String number, int k) {
+        /*
+        number 범위 및 k 보니까 완전 탐색 같은 건 안 될 듯 n으로 끝나야 함?
+        앞에 작은 수가 있으면 그걸 뺀다?
+        스택 사용? k만큼 뺄 때까지
+        */
         Stack<Integer> stack = new Stack<>();
         
         for (int i = 0; i < number.length(); i++) {
-            while (k > 0 && !stack.isEmpty() && stack.peek() < number.charAt(i) - '0') {
+            while (!stack.isEmpty() && k > 0 && stack.peek() < number.charAt(i) - '0') {
                 stack.pop();
                 k--;
             }
             stack.push(number.charAt(i) - '0');
         }
+        
+        StringBuilder sb = new StringBuilder("");
         while (k > 0) {
             stack.pop();
             k--;
         }
-        StringBuilder sb = new StringBuilder("");
         while (!stack.isEmpty()) {
             sb.append(stack.pop());
         }
