eb659306437387aae89ec5fec5c7a49eaf160f1a
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-04-05 12:16:29 +0900

    [level 2] Title: 구명보트, Time: 11.25 ms, Memory: 57.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        Arrays.sort(people);
-        
         int answer = 0;
         int left = 0;
         int right = people.length - 1;
+        
+        Arrays.sort(people);
         while (left <= right) {
             if (people[left] + people[right] <= limit) {
                 left++;
