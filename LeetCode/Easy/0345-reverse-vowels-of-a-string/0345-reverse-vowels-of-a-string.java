f544c73c95b0d686295a30865b132c4ea414fb46
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-04-15 10:38:08 +0900

    0345-reverse-vowels-of-a-string [Easy] | Topic: Two Pointers | Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ LeetCode/Easy/0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,31 +1,22 @@
 class Solution {
     public String reverseVowels(String s) {
-        /**
-        모음들의 순서를 뒤집기
-        자음은 앞에서부터 걍 추가하고
-        모음은 뒤에서부터 온다?
-        앞에서부터 넣다가 모음 발견하면 뒤에 첫 번째 모음을 넣고 앞으로 옮긴다.
-        */
         String vowels = "aeiouAEIOU";
-        int vowelNum = s.length();
-        while (true) {
-            vowelNum--;
-            if (vowelNum < 0 || vowels.contains(Character.toString(s.charAt(vowelNum)))) break;
-        }
-        StringBuilder sb = new StringBuilder("");
+        char[] arr = s.toCharArray();
+        int left = 0;
+        int right = s.length() - 1;
 
-        for (int i = 0; i < s.length(); i++) {
-            if (vowels.contains(Character.toString(s.charAt(i)))) {
-                sb.append(s.charAt(vowelNum));
-                while (true) {
-                    vowelNum--;
-                    if (vowelNum < 0 || vowels.contains(Character.toString(s.charAt(vowelNum)))) break;
-                }
-            } else {
-                sb.append(s.charAt(i));
-            }
+        while (left < right) {
+            while (left < right && !vowels.contains(Character.toString(arr[left]))) left++;
+            while (left < right && !vowels.contains(Character.toString(arr[right]))) right--;
+
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            
+            left++;
+            right--;
         }
 
-        return sb.toString();
+        return new String(arr);
     }
 }
