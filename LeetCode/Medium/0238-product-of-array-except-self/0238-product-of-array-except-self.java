aaf6d4853177f3aa7dc9418f31e4a3e79d5c70d7
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-04-17 13:37:58 +0900

    0238-product-of-array-except-self [Medium] | Topic: Array | Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -7,28 +7,27 @@ class Solution {
         O(n)으로 끝내야하니까 각 인덱스마다 바로 구해야한다?
         0이 두 개 이상 있으면 다 0이고
         0이 하나 있는 경우 그 0 하나를 찾아야하는데
+        나눗셈을 쓰지 말고 하라고??
+        왼쪽 오른쪽 나눠서..
         */
-        int zeroCount = 0;
-        int zeroIndex = 0;
-        int prod = 1;
+        
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] result = new int[n];
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                zeroCount++;
-                zeroIndex = i;
-            } else {
-                prod *= nums[i];
-            }
+        left[0] = 1;
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
         }
 
-        int[] result = new int[nums.length];
+        right[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
 
-        if (zeroCount == 1) {
-            result[zeroIndex] = prod;
-        } else if (zeroCount == 0) {
-            for (int i = 0; i < nums.length; i++) {
-                result[i] = prod / nums[i];
-            }
+        for (int i = 0; i < n; i++) {
+            result[i] = left[i] * right[i];
         }
 
         return result;
