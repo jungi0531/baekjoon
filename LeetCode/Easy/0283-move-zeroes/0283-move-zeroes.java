17da3a057f90ab8c1a56e7a2d454383b4378c23b
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-04-21 14:56:30 +0900

    0283-move-zeroes [Easy] | Topic: Array | Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.java
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.java
@@ -3,12 +3,14 @@ class Solution {
         /**
         버블 소트 느낌으로 0 오른쪽으로 밀기
         */
+        
+        int p = 0;
 
-        for (int i = 1; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) continue;
-            int ii = i - 1;
-            while (ii - 1 >= 0 && nums[ii - 1] == 0) ii--;
-            if (nums[ii] == 0) swap(nums, i, ii);
+
+            swap(nums, i, p);
+            p++;
         }
     }
     public void swap(int[] nums, int a, int b) {
