9bbeaf5988246524b0aee01eab49ea7dd72932e0
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-04-23 17:14:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 92.06 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,24 +2,23 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        // 동명이인 있을 수 있으니 HashMap으로 사람 수까지 카운팅 하고
-        // 걍 HashMap에 넣어서 contains 하고 있으면 -- 해야겠네
-        String answer = "";
-        HashMap<String, Integer> hash = new HashMap<>();
+        /**
+        맵에다가 개수까지 넣어서 없애야함
+        */
+        HashMap<String, Integer> map = new HashMap<>();
         
         for (int i = 0; i < participant.length; i++) {
-            hash.put(participant[i], hash.getOrDefault(participant[i], 0) + 1);
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
         }
         for (int i = 0; i < completion.length; i++) {
-            if (hash.containsKey(completion[i])) {
-                hash.put(completion[i], hash.get(completion[i]) - 1);
-                if (hash.get(completion[i]) == 0) {
-                    hash.remove(completion[i]);
-                }
-            }
+            map.put(completion[i], map.get(completion[i]) - 1);
+            if (map.get(completion[i]) == 0) map.remove(completion[i]);
         }
-        for (String temp : hash.keySet()) {
-            return temp;
+        
+        String answer = "";
+        
+        for (String temp : map.keySet()) {
+            answer = temp;
         }
         
         return answer;
