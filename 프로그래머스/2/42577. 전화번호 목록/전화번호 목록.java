408725e7bfdb83542f31b91336a6024b36fc847c
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-04-23 17:44:06 +0900

    [level 2] Title: 전화번호 목록, Time: 295.42 ms, Memory: 247 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,15 +2,21 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        /**
+        접두어가 있으면 다른 거 반환
+        반복문 돌면서 전화번호 다 넣은 해시셋에 그게 있는지 확인
+        */
         HashSet<String> hash = new HashSet<>();
         
         for (int i = 0; i < phone_book.length; i++) {
             hash.add(phone_book[i]);
         }
         
-        for (String temp : hash) {
-            for (int i = 1; i < temp.length(); i++) {
-                if (hash.contains(temp.substring(0, i))) return false;
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int ii = 1; ii < phone_book[i].length(); ii++) {
+                if (hash.contains(phone_book[i].substring(0, ii))) {
+                    return false;
+                }
             }
         }
         
