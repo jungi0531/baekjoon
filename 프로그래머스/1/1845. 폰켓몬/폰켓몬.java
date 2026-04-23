4fe4997e5ad5c07d1acdcc939c31f0520fdf6457
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-04-23 17:29:16 +0900

    [level 1] Title: 폰켓몬, Time: 5.49 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,14 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        // 해시맵에 다 넣고 answer = Math.max(nums.length, hash.size());
-        HashMap<Integer, Integer> hash = new HashMap<>();
+        /**
+        N마리 중 N / 2마리를 고르는데 최대한 다양한 종류의 포켓몬을 고르려면
+        종류를 체크하고 Math.min(종류, N/2) 출력?
+        */
+        int answer = 0;
+        HashSet<Integer> hash = new HashSet<>();
+        
         for (int i = 0; i < nums.length; i++) {
-            hash.put(nums[i], hash.getOrDefault(nums[i], 0) + 1);
+            hash.add(nums[i]);
         }
         
-        int answer = Math.min(nums.length / 2, hash.size());
-        
-        return answer;
+        return Math.min(hash.size(), nums.length / 2);
     }
 }
