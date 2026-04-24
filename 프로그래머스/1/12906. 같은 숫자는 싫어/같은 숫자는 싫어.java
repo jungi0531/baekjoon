0d91db01c1f721251763e976b9de806bbfed423a
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-04-24 12:36:38 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 33.26 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,28 +2,19 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        int[] answer = {};
-        Deque<Integer> q = new ArrayDeque<>();
-        
+        /**
+        한 숫자가 나오면 다른 숫자가 나올 때까지 스킵
+        */
+        Queue<Integer> q = new LinkedList<>();
         for (int i = 0; i < arr.length; i++) {
-            if (q.isEmpty()) {
-                q.add(arr[i]);
-            }
-            else {
-                if (q.peekLast() != arr[i]) {
-                    q.add(arr[i]);
-                }
-            }
-        }
-        int i = 0;
-        answer = new int[q.size()];
-        while (!q.isEmpty()) {
-            answer[i++] = q.poll();
+            q.add(arr[i]);
+            while (i + 1 < arr.length && arr[i] == arr[i + 1]) i++;
         }
         
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
+        int[] answer = new int[q.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = q.poll();
+        }
         return answer;
     }
 }
