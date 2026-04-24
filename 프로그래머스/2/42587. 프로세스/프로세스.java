20ce1c2fe02c0a247ac540d48ddba5d52c86652f
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-04-24 14:25:07 +0900

    [level 2] Title: 프로세스, Time: 1.82 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,32 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        // 흠 리얼 그냥 그대로 하는 건가..
-        // 큐랑 피큐에 둘다 넣고 현재 뺀 게 피큐 맨 위에 있는지 확인?
-        Queue<Integer> q = new LinkedList<>();
-        Queue<Boolean> isLocation = new LinkedList<>();
+        /**
+        젤 높은 우선 순위가 아니면 꺼내져도 다시 넣어야 함
+        우선순위 큐를 만들고 그거에 값을 다 넣어놓고 peek() 값이랑 지금 값이랑 같으면 빼고
+        */
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        Queue<int[]> q = new LinkedList<>();
         for (int i = 0; i < priorities.length; i++) {
-            if (i == location) isLocation.add(true);
-            else isLocation.add(false);
-            q.add(priorities[i]);
             pq.add(priorities[i]);
+            if (i == location) q.add(new int[]{priorities[i], 1});
+            else q.add(new int[]{priorities[i], 0});
         }
-        
-        int answer = 1;
+        //
+        int count = 0;
         while (!q.isEmpty()) {
-            if (q.peek() == pq.peek()) {
-                if (isLocation.peek()) break;
-                q.poll();
-                isLocation.poll();
+            int[] remove = q.poll();
+            if (remove[0] == pq.peek()) {
+                count++;
+                if (remove[1] == 1) return count;
                 pq.poll();
-                answer++;
             } else {
-                q.add(q.poll());
-                isLocation.add(isLocation.poll());
+                q.add(remove);
             }
         }
-        
-        return answer;
+        //
+        return count;
     }
 }
