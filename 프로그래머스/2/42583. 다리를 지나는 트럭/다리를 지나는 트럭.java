e61919ca61691c1b805edd367bbee24121b262c4
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-04-24 17:06:30 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 13.63 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,39 +2,37 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        // 다리 큐에 있는 트럭의 총 합이 weight 보다 작아야 함
-        // 또한 트럭의 개수가 다리 길이보다 넘지 못 함
+        /**
+        다리에 있는 트럭을 큐로 관리하고 무게는 따로 관리해서 다리를 관리.
+        트럭의 무게와 드가는 시간까지 같이 넣기
+        드갈 때 현재 시간 넣고 뺄 때 현재 시간 - 드간 시간 >= 다리 길이면 빼기
+        */
         Queue<int[]> bridge = new LinkedList<>();
-        int sum = 0;
-        int time = 0;
-        int i = 0;
-        while (i < truck_weights.length) {
-            time++;
-            
-            // 추가하면서 동시에 빠질 수도 있음
-            // 언제 빠지는데? 현재 시간 - 들온 시간이 다리 길이일 때 <- 들온 시간을 알아야 함
-            // 빠지면서 들어가니까 빠지는 거 먼저
-            // 다리에 있던 시간이 다리 길이보다 커야함
-            if (!bridge.isEmpty() && time - bridge.peek()[0] >= bridge_length) {
-                int[] remove = bridge.poll();
-                sum -= remove[1];
-                time = remove[0] + bridge_length;
-            }
-            
-            // 빠지고 나서 들어가기.
-            if (sum + truck_weights[i] <= weight) {
-                // 추가해도 견딜 수 있는 무게면
-                bridge.add(new int[]{time, truck_weights[i]});
-                sum += truck_weights[i];
-                i++;
-            }
-        }
-        // 트럭이 다 다리 안에 있으면 마지막에 나온 트럭의 time + 다리 길이
-        while (!bridge.isEmpty()) {
-            int[] remove = bridge.poll();
-            time = remove[0] + bridge_length;
-        }
         
-        return time;
+        int count = 0;
+        int time = 0;
+        int total = 0;
+        while (count < truck_weights.length) {
+            // 빼기
+            if (!bridge.isEmpty()) {
+                if (time - bridge.peek()[1] >= bridge_length) {
+                    int[] remove = bridge.poll();
+                    total -= remove[0];
+                }
+            }
+            // 넣기
+            if (total + truck_weights[count] <= weight) {
+                bridge.add(new int[]{truck_weights[count], time});
+                total += truck_weights[count];
+                count++;
+            }
+            //
+            time++;
+        }
+        // 큐 다 빼기
+        for (int[] temp : bridge) {
+            time += bridge_length - (time - temp[1]);
+        }
+        return time + 1;
     }
 }
