da6c770a75da0cfffa6468b24bd3fb07629c3b19
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-05-08 14:48:22 +0900

    0724-find-pivot-index [Easy] | Topic: Array | Time: 1 ms (98.62%), Space: 47.5 MB (39.59%) - LeetHub

---
+++ LeetCode/Easy/0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        /**
+        하나 중간에 비우고 양 옆의 합이 같도록 하는 인덱스. 없으면 -1 반환
+        비우는 인덱스를 첨부터 끝까지 구하기
+        */
+        int leftSum = 0;
+        int rightSum = 0;
+        int index = 0;
+        for (int i = 1; i < nums.length; i++) {
+            rightSum += nums[i];
+        }
+
+        while (index < nums.length - 1 && leftSum != rightSum) {
+            leftSum += nums[index];
+            index++;
+            rightSum -=nums[index];
+        }
+
+        if (index >= nums.length - 1 && leftSum != rightSum) return -1;
+        return index;
+    }
+}
