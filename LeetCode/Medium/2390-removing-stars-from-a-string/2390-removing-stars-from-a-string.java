76eb94456f519603a209e68609087b859d5a456c
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-05-13 10:31:48 +0900

    2390-removing-stars-from-a-string [Medium] | Topic: String | Time: 84 ms (26.34%), Space: 48.8 MB (28.7%) - LeetHub

---
+++ LeetCode/Medium/2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String removeStars(String s) {
+        /**
+        별이 나오면 지우는데 이전에 나온 것도 지우기
+        */
+        Stack<Character> stack = new Stack<>();
+        //
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '*') {
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+        //
+        StringBuilder sb = new StringBuilder("");
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
