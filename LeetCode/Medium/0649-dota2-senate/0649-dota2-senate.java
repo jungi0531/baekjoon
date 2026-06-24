289b15eaa99d113422a0735b139bda84f761398a
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-06-24 09:48:35 +0900

    0649-dota2-senate [Medium] | Topic: String | Time: 11 ms (84.43%), Space: 46.6 MB (75.37%) - LeetHub

--- LeetCode/Medium/0649-dota2-senate/0649-dota2-senate.java
+++ LeetCode/Medium/0649-dota2-senate/0649-dota2-senate.java
@@ -6,8 +6,9 @@ class Solution {
         // R과 D에 해당하는 큐를 각각 만들고 큐에 뭐가 있으면 하나 빼고 스킵 아니면 공격 가능
         // 하나만 있는 경우도 봐야함..
         Queue<Integer> q = new LinkedList<>();
-        Queue<Integer> r = new LinkedList<>();
-        Queue<Integer> d = new LinkedList<>();
+
+        int r = 0;
+        int d = 0;
 
         int rCount = 0;
         int dCount = 0;
@@ -25,20 +26,20 @@ class Solution {
         
         while (q.size() > 1 && (rCount > 0 && dCount > 0)) {
             if (q.peek() == 0) {
-                if (d.isEmpty()) {
-                    r.add(0);
+                if (d == 0) {
+                    r++;
                     q.add(q.poll());
                 } else {
-                    d.poll();
+                    d--;
                     q.poll();
                     rCount--;
                 }
             } else {
-                if (r.isEmpty()) {
-                    d.add(0);
+                if (r == 0) {
+                    d++;
                     q.add(q.poll());
                 } else {
-                    r.poll();
+                    r--;
                     q.poll();
                     dCount--;
                 }
