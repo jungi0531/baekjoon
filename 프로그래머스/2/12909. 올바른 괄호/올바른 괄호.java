e27915ecd46b6aa160d24750c59d69b668905c5a
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-09-03 13:44:34 +0900

    [level 2] Title: 올바른 괄호, Time: 12.99 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -5,14 +5,38 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
         
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') stack.push(0);
-            else {
+            if (s.charAt(i) == '(') {
+                stack.push(0);
+            } else {
                 if (stack.isEmpty()) return false;
                 stack.pop();
             }
         }
         
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+        
         if (!stack.isEmpty()) return false;
         return true;
     }
 }
+
+// import java.util.*;
+
+// class Solution {
+//     boolean solution(String s) {
+//         Stack<Integer> stack = new Stack<>();
+        
+//         for (int i = 0; i < s.length(); i++) {
+//             if (s.charAt(i) == '(') stack.push(0);
+//             else {
+//                 if (stack.isEmpty()) return false;
+//                 stack.pop();
+//             }
+//         }
+        
+//         if (!stack.isEmpty()) return false;
+//         return true;
+//     }
+// }
