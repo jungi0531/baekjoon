c1ed90b0a802647c39fbfc4d1ad9231f7449f668
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-09-03 13:55:26 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 2.96 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,16 +1,32 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
+        
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
+        String[] arr = s.split(" ");
         
-        String[] input = s.split(" ");
-        for (int i = 0 ; i < input.length; i++) {
-            max = Math.max(max, Integer.parseInt(input[i]));
-            min = Math.min(min, Integer.parseInt(input[i]));
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, Integer.parseInt(arr[i]));
+            min = Math.min(min, Integer.parseInt(arr[i]));
         }
-        answer = Integer.toString(min) + " " + Integer.toString(max);
         
-        return answer;
+        return min + " " + max;
     }
 }
+
+// class Solution {
+//     public String solution(String s) {
+//         String answer = "";
+//         int max = Integer.MIN_VALUE;
+//         int min = Integer.MAX_VALUE;
+        
+//         String[] input = s.split(" ");
+//         for (int i = 0 ; i < input.length; i++) {
+//             max = Math.max(max, Integer.parseInt(input[i]));
+//             min = Math.min(min, Integer.parseInt(input[i]));
+//         }
+//         answer = Integer.toString(min) + " " + Integer.toString(max);
+        
+//         return answer;
+//     }
+// }
