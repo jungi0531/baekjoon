e17b36d03cb79c8babccec85d149823f77f524eb
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-09-04 16:43:27 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.07 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,25 +1,44 @@
+import java.util.*;
+
 class Solution {
     public String solution(String s) {
+        // 이 전이 공백 문자인 첫 문자 알파벳을 대문자로, 나머지는 소문자로.
         StringBuilder sb = new StringBuilder("");
-        String answer = "";
         
-        if (Character.isAlphabetic(s.charAt(0))) {
-            sb.append(Character.toUpperCase(s.charAt(0)));
-        } else {
-            sb.append(s.charAt(0));
-        }
+        s = s.toLowerCase();
         
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i - 1) == ' ' && Character.isAlphabetic(s.charAt(i))) {
+        for (int i = 0; i < s.length(); i++) {
+            if (i == 0 || 
+                (s.charAt(i - 1) == ' ' &&
+                Character.isLetter(s.charAt(i)))) {
                 sb.append(Character.toUpperCase(s.charAt(i)));
-            } else if (Character.isAlphabetic(s.charAt(i))) {
-                sb.append(Character.toLowerCase(s.charAt(i)));
-            } else {
-                sb.append(s.charAt(i));
-            }
+            } else sb.append(s.charAt(i));
+            
         }
-        answer = sb.toString();
-        
-        return answer;
+        return sb.toString();
     }
 }
+
+// class Solution {
+//     public String solution(String s) {
+//         StringBuilder sb = new StringBuilder("");
+//         boolean isFirst = true;
+        
+//         for (char c : s.toCharArray()) {
+//             if (c == ' ') {
+//                 isFirst = true;
+//                 sb.append(c);
+//             } else {
+//                 if (isFirst) {
+//                     isFirst = false;
+//                     sb.append(Character.toUpperCase(c));
+//                 } else {
+//                     sb.append(Character.toLowerCase(c));
+//                 }
+//             }
+//         }
+        
+        
+//         return sb.toString();
+//     }
+// }
