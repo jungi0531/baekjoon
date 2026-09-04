45ffdb8b14edd324caaa6e0d27914c6ba5139b5b
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-09-04 16:19:33 +0900

    [level 2] Title: 최솟값 만들기, Time: 9.38 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -4,11 +4,15 @@ class Solution
 {
     public int solution(int []A, int []B)
     {
+        // 곱한 값이 최소가 되려면 최솟값과 최댓값이 곱해져야함
         int answer = 0;
 
-        // 최소가 되게 하려면 최솟값 * 최댓값으로 해야한다 그러려면 A는 오름차순 정렬, B는 내림차순 정렬을 하고 순서대로 곱하면 될 듯
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+        
         Arrays.sort(A);
         Arrays.sort(B);
+        
         for (int i = 0; i < A.length; i++) {
             answer += A[i] * B[B.length - (i + 1)];
         }
@@ -16,3 +20,22 @@ class Solution
         return answer;
     }
 }
+
+// import java.util.*;
+
+// class Solution
+// {
+//     public int solution(int []A, int []B)
+//     {
+//         int answer = 0;
+
+//         // 최소가 되게 하려면 최솟값 * 최댓값으로 해야한다 그러려면 A는 오름차순 정렬, B는 내림차순 정렬을 하고 순서대로 곱하면 될 듯
+//         Arrays.sort(A);
+//         Arrays.sort(B);
+//         for (int i = 0; i < A.length; i++) {
+//             answer += A[i] * B[B.length - (i + 1)];
+//         }
+
+//         return answer;
+//     }
+// }
