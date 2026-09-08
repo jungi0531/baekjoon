02e9232eeb02b7a0e05ff879393a9b9ba61802b4
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-09-08 13:57:06 +0900

    [level 2] Title: 숫자의 표현, Time: 1.09 ms, Memory: 57.5 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,9 +1,17 @@
 class Solution {
     public int solution(int n) {
+        // n을 연속된 자연 수의 합으로 표현하는 방법
         int answer = 0;
-        for (int i = 1; i <= n; i += 2) { // 홀수만 확인
-            if (n % i == 0) answer++;
+        
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int ii = i; ii <= n; ii++) {
+                sum += ii;
+                if (sum == n) answer++;
+                if (sum >= n) break;
+            }
         }
+        
         return answer;
     }
 }
