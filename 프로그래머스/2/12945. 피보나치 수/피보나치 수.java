2d1908eb6e437f9ee83bce8eded61aa310381c90
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-09-14 16:48:44 +0900

    [level 2] Title: 피보나치 수, Time: 2.77 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int[] fibo = new int[n + 1];
+        int[] fibo = new int[100_001];
         fibo[0] = 0;
         fibo[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 1234567;
+        for (int i = 2; i <= 100_000; i++) {
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+            fibo[i] %= 1234567;
         }
         
         return fibo[n];
