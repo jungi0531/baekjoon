404b577abdf45b7b76cea9d3a5a15567feea52c3
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-09-18 12:04:51 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,19 +1,20 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
+        // 테두리는 갈색, 내부는 노란색. 각각 개수가 주어질 때 총 크기를 구해라.
+        // 갈색 노란색 칸의 합이 총 개수의 곱이랑 같다.
+        // 가로 세로 최소 3부터..
         
-        int total = brown + yellow;
-        for (int i = 3; i < total; i++) {
-            if (total % i == 0) {
-                int x = total / i;
-                if ((x - 2) * (i - 2) == yellow) {
-                    answer[0] = x;
-                    answer[1] = i;
-                    break;
+        int sum = brown + yellow;
+        
+        for (int i = 3; i < sum; i++) {
+            if (sum % i == 0) {
+                int x = i;
+                int y = sum / i;
+                if ((x - 2) * (y - 2) == yellow) {
+                    return new int[]{y, x};
                 }
             }
         }
-        
-        return answer;
+        return new int[]{};
     }
 }
