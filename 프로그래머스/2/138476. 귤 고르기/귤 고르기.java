8b44394651df01890752f6636ceea487166e91e6
Jungi Hong <152581245+jungi0531@users.noreply.github.com>
2026-09-18 12:16:02 +0900

    [level 2] Title: 귤 고르기, Time: 43.08 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -2,21 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
-        int answer = 0;
-        Map<Integer, Integer> map = new HashMap<>();
+        // 개수 가장 많은 순으로 정렬하기
+        // 해시맵에 key 값은 귤 크기, value 값은 개수
+        HashMap<Integer, Integer> map = new HashMap<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         
         for (int i = 0; i < tangerine.length; i++) {
             map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
         }
-        List<Integer> list = new ArrayList<>(map.values());
-        
-        list.sort(Collections.reverseOrder());
-        
-        while (k > 0) {
-            k -= list.get(answer);
-            answer++;
+        for (Integer temp : map.keySet()) {
+            pq.add(map.get(temp));
         }
         
-        return answer;
+        int count = 0;
+        
+        while (k > 0) {
+            k -= pq.poll();
+            count++;
+        }
+        
+        return count;
     }
 }
